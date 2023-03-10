27fbfd3c6e13bf575829be4bb2e36e521786bcab
riverify <halo@ahnu.edu.cn>
2023-03-10 23:27:10 +0800

    快速排序

--- chapter_1_basics/src/com/riverify/sort/quick_sort/QuickSort.java
+++ chapter_1_basics/src/com/riverify/sort/quick_sort/QuickSort.java
@@ -20,14 +20,14 @@ public class QuickSort {
         if (l >= h) {
             return; // 递归结束点
         }
-        int p = partition(a, l, h); // p基准点的索引
+        int p = paratition2(a, l, h); // p基准点的索引
         quick(a, l, p - 1);  // 左边分区的范围确定
         quick(a, p + 1, h);   // 右边分区的范围确定
     }
 
+
     /**
      * Lomuto单边快排分区代码
-     *
      * @param a array
      * @param l low
      * @param h high
@@ -49,6 +49,40 @@ public class QuickSort {
         int temp = a[i];
         a[i] = a[h];
         a[h] = temp;
+
+        System.out.println(Arrays.toString(a) + ", i = " + i + ", j = " + j);
+
+        return i;
+    }
+
+
+    /**
+     * 双边循环快排
+     */
+    public static int paratition2(int[] a, int l, int h) {
+        int pv = a[l];
+        int i = l;
+        int j = h;
+        // 只要i小于j，循环一直进行
+        while (i < j) {
+            // j从右找比pv小的，当j停下的时候，指向的数是比基准点pv小的值
+            while (i < j && a[j] > pv) {
+                j--;
+            }
+            // i从左找比pv大的，当i停下的时候，指向的数是比基准点pv大的值
+            while (i < j && a[i] <= pv) {
+                i++;
+            }
+            // 交换i，j所指向的数
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        // 退出while循环的时候，i和j已经相等了，此时基准点与i或者j交换位置
+        int temp = a[i];
+        a[i] = a[l];
+        a[l] = temp;
+
         System.out.println(Arrays.toString(a) + ", i = " + i + ", j = " + j);
 
         return i;
