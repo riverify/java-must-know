8bc8e121dc8f6be5b8ed9fc4da7cf04fd50f078c
riverify <halo@ahnu.edu.cn>
2023-03-10 12:02:18 +0800

    二分查找 整数溢出问题

--- chapter_1_basics/src/com/riverify/binary_search/IntegerOverflow.java
+++ chapter_1_basics/src/com/riverify/binary_search/IntegerOverflow.java
@@ -7,4 +7,27 @@ package com.riverify.binary_search;
  * Author: river
  */
 public class IntegerOverflow {
+    public static void main(String[] args) {
+        int l = 0;
+        int r = Integer.MAX_VALUE - 1;
+
+        int m = (l + r) / 2;
+        System.out.println(m);      // 1073741823
+
+        // 假若target在中间值右侧
+//        l = m + 1;
+//        m = (l + r) / 2;
+//        System.out.println(m);      // -536870913  由于溢出导致的负数
+
+//
+//        // 改进 m 表达式
+//        l = m + 1;
+//        m = l + ( r - l ) / 2;
+//        System.out.println(m);
+
+        // 改进2: 无符号的右移运算
+        l = m + 1;
+        m = (l + r) >>> 1;
+        System.out.println(m);
+    }
 }
